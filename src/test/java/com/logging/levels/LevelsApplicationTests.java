6ba8252cb72b95897fed0cd1a6e4a0ce1f0fe2f5
krish13raj <krishsnr@gmail.com>
2024-10-30 11:45:43 +0530

    first commit

---
+++ src/test/java/com/logging/levels/LevelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logging.levels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LevelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
